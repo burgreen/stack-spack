4502df570c166d44cae180e289432eacb9f556c6
burgreen <greg.burgreen@msstate.edu>
2019-02-11 17:49:53 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# stack-spack
+stack-spack
+===========
+
+This project contains bash scripts to setup a Spack-based software stack and find a suitable system compiler to continue building the stack.
+
+Useful information before beginning
+-----------------------------------
+
+Spack works only on Linux and MacOS platforms. Windows is [not supported](https://github.com/spack/spack/issues/1515).
+
+Before installing Spack, make sure your system supports [Environment Modules](http://modules.sourceforge.net/) package. It can be easily [installed](https://modules.readthedocs.io/en/stable/INSTALL.html).
+
+Ensure your system has the minimum Spack [prerequisites](https://spack.readthedocs.io/en/latest/getting_started.html#prerequisites). 
+
+Installation
+------------
+
+`$ ./install-spack.sh <stack_dir>`
+
+where `<stack_dir>` is path of the desired software stack.
+
+Finding suitable system compilers that install Spack packages 
+-------------------------------------------------------------
+
+- In our limited experience, many system compilers fail for various reasons to install the full Proteus stack. 
+
+- On Linux systems, the approach that has been most consistent for us has been to build `gcc-7.3.0` and `binutils` using a suitable system compiler. Then, we use the installed `Spack gcc-7.3.0` to build the remaining software stack.
+
+- One can use a `system gcc-7.3.0` to build the `Spack gcc-7.3.0`. In fact, we had to do this very thing in the past in order to reliably build some ornery software stacks on our systems. 
+
+- On MacOS systems, one can build `gcc-7.3.0` using a `clang` compiler, but `gcc` on a Mac does not play well with `cmake` and other packages. 
+
+**Be advised: 
+A system compiler may succeed in installing the small package (libelf) related to try-compiler.sh, but may fail for various other reasons during the build of the more complex stack packages. If this happens, setup another system compiler, and try again.**
+
+- Below is a typical work flow that selects gcc-7.3.0 as its final system compiler.
+```
+$ cd <stack_dir>
+
+$ ./scripts/compiler.sh add
+$ ./scripts/compiler.sh avail
+
+$ ./scripts/try-compiler.sh try gcc@4.4.7
+
+$ module load gcc/4.9.3
+$ ./scripts/compiler.sh add gcc@4.9.3
+$ ./scripts/compiler.sh try gcc@4.9.3
+
+$ module load gcc/7.3.0
+$ ./scripts/compiler.sh add gcc@7.3.0.
+$ ./scripts/compiler.sh try gcc@7.3.0.
+$ ./scripts/compiler.sh finalize gcc@7.3.0.
+```
---
+++ install-spack.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+#set -x 
+set -e 
+
+#--------------------------------------------
+# purpose: install spack and basic setup
+#
+# author: greg.burgreen@msstate.edu
+#         Mississippi State Univ
+#         version 2019.02.11
+#--------------------------------------------
+
+if [ -z "$1" ]; then 
+  echo usage: $0 \<spack_dir\>
+  echo where \<spack_dir\> is the preferred name of your spack repo
+  echo 
+  echo For example, $0 ../stack-2019.02.11
+  exit
+fi
+
+current=$(pwd)
+
+spack_archive=spack-0.12.1
+
+pkg_libelf=sources-thirdparty-libelf.tar
+scripts=scripts.tar
+
+#--------------------------------------------
+# 1. prelims
+#--------------------------------------------
+
+if [ -d $1 ]; then
+   echo Directory $1 already exists.
+   exit
+fi
+
+#source ./spack-setup-1-names-proteus.sh
+
+mkdir -p $1
+
+cp $current/0-README.md $1/0-README.md
+
+cat >> $1/0-README.md << EOF
+
+spack_archive = $spack_archive
+EOF
+
+#--------------------------------------------
+# 2. write file: 3-names.sh
+#--------------------------------------------
+
+cat > _names << EOF
+#!/bin/bash
+
+EOF
+
+chmod +x _names
+
+mv _names $1/3-names.sh
+
+#--------------------------------------------
+# 3. unarchive spack_archive
+#--------------------------------------------
+
+echo Installing spack source: $spack_archive.tar.gz
+
+tar xfz $spack_archive.tar.gz
+#unzip -o $spack_archive.zip
+
+mv $spack_archive/* $1
+mv $spack_archive/.[a-z]* $1
+rmdir $spack_archive
+
+cd $1
+
+#--------------------------------------------
+# 4. unarchive tarballs
+#--------------------------------------------
+
+echo Installing other tarballs
+
+tar xf $current/$pkg_libelf
+tar xf $current/$scripts
+
+#--------------------------------------------
+# 5. write file: 1-setup-spack.sh
+#--------------------------------------------
+
+cat > 1-setup-spack.sh << EOF
+#!/bin/bash
+
+export SPACK_ROOT=$(pwd)
+
+source \$SPACK_ROOT/share/spack/setup-env.sh
+EOF
+
+source ./1-setup-spack.sh
+
+#--------------------------------------------
+# 6. write file: mirrors.yaml
+#--------------------------------------------
+
+cat > $SPACK_ROOT/etc/spack/mirrors.yaml << EOF
+mirrors:
+  local_filesystem: file://$SPACK_ROOT/thirdparty
+EOF
+
+#--------------------------------------------
+# 6. write instructions
+#--------------------------------------------
+
+echo ----------------------------------------
+echo $spack_archive has been installed.
+echo
+echo Next steps:
+echo 1. Change directory to ./$1
+echo 2. Follow instructions in 0-README.txt
+
+set +e
+#set +x 
